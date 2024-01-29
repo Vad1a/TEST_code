db0c8642a7945521082c439ca796cf6dcf81652e
losjein <158050763+losjein@users.noreply.github.com>
2024-01-29 15:22:36 +0300

    666

---
+++ test.cpp
@@ -0,0 +1,32 @@
+/* 01-03.c демонстрация результатов инкремента
+ в разных ситуациях */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int i = 0, a = 0;
+ printf("\n i = %d, a = %d\n", i,a);
+ a = ++i;
+ printf("a = ++i i = %d, a = %d\n", i,a);
+ a = i++;
+ printf("a = i++ i = %d, a = %d\n", i,a);
+ /*
+ i=0;a=0;
+ printf("i=0;a=0; i = %d, a = %d\n", i,a);
+ */
+ a = ++i + ++i ;
+ printf("a = ++i + ++i i = %d, a = %d\n", i,a);
+ a = i++ + i++;
+ printf("a = i++ + i++ i = %d, a = %d\n", i,a);
+ a = ++i + ++i + ++i;
+11
+ printf("a = ++i + ++i + ++i i = %d, a = %d\n", i,a);
+ a = i++ + i++ + i++;
+ printf("a = i++ + i++ + i++ i = %d, a = %d\n", i,a);
+ a = a + --i;
+ printf("a = a + --i i = %d, a = %d\n", i,a);
+ a = a + i--;
+ printf("a = a + i-- i = %d, a = %d\n", i,a);
+ getch();
+ return 0;
+}
